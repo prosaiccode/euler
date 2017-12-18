1d2eaba6d7272ea9084836ebcd1026a6e700f9d7
prosaiccode <33367893+prosaiccode@users.noreply.github.com>
2017-12-18 13:23:31 -0800

    Update p002.py

--- src/p002.py
+++ src/p002.py
@@ -2,12 +2,15 @@
 Find n of fibonacci(n)
 
 limit = fibonacci(n)
-n = math.log(limit * 5 ** .5) / (math.log(golden ratio) - math.log(1/golden ratio) * 2
+n = math.log(limit * 5 ** .5) / (math.log((1 + 5 ** .5) / 2) - math.log(2 / (1 + 5 ** .5)) * 2
 The two at the end is because of the power is taken twice.
 
 Ranges of n:
-  0-71: exact
-  72- : possible to be larger than the actual value by exactly .9% [still trying to find exact but this is close] (floor)
+  Possible to off by 1 due to rounding.
+  It's assumed that `limit` is larger than 1. Since fibonacci(1) and fibonacci(2) are both 1.
+  n can't be larger than 1474. Python rounds math.log(fibonacci(1475) * 5 ** .5) to infinity.
+  
+  The program works through to 8.039528104473e+307  (fibonacci(1475) = 1e+308)
 """
 from math import log
 
