b721da7fef1c712becd7a2609294e6a321ecdc06
prosaiccode <33367893+prosaiccode@users.noreply.github.com>
2017-12-19 13:39:30 -0800

    emergency commit

--- src/p002.py
+++ src/p002.py
@@ -11,6 +11,32 @@ Ranges of n:
   n can't be larger than 1474. Python rounds math.log(fibonacci(1475) * 5 ** .5) to infinity.
   
   The program works through to 8.039528104473e+307  (fibonacci(1475) = 1e+308)
+  
+sum(fibonacci(0),fibonacci(n)) = fibonacci(n+2) - fibonacci(2)
+sum(lucas(1),lucas(n)) = lucas(n+2) - lucas(2)
+
+Only even values:
+  if f1 and f2 are even: count all
+  else if f1 even and not f2: count f1 + f4 + f7 + f10 ...
+  else if f2 even and not f1: count f2 + f5 + f8 + f11 ...
+  else: count f3 + f6 + f9 + f12 ...
+
+elses' formula for even values: 4 * f(n-1) + f(n-2)
+  recursive sum: sigma from x=1 to k=n//3
 """
 from math import log
+from sys import argv
 
+def __init__(limit, f1=1, f2=2):
+  
+
+def fibonacci(n, f1=1, f2=1):
+  A = [[f1, f2], [f2, f2-f1]]
+  
+  
+
+def fib_n(v):
+  return 2 * log(v * 5 ** .5) / (math.log( (1 + 5 ** .5) / 2) - math.log(2 / (1 + 5 ** .5))
+
+if __naim__ == '__main__':
+    __init__(4000000)
